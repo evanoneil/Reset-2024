5633cee899a18a391222c19dca895d0c7794bf80
Evan O'Neil <evanoneil@windows-2000.lan>
2024-03-18 15:36:16 -0500

    initial updates

--- app/src/lib/sanity/api.ts
+++ app/src/lib/sanity/api.ts
@@ -1,6 +1,8 @@
 import {
 	PUBLIC_SANITY_DATASET,
-	PUBLIC_SANITY_PROJECT_ID
+	PUBLIC_SANITY_PROJECT_ID,
+	PUBLIC_SANITY_API_VERSION,
+	PUBLIC_SANITY_STUDIO_URL
 } from '$env/static/public';
 
 export function assertEnvVar<T>(value: T | undefined, name: string): T {
@@ -13,3 +15,7 @@ export function assertEnvVar<T>(value: T | undefined, name: string): T {
 export const dataset = assertEnvVar(PUBLIC_SANITY_DATASET, 'PUBLIC_SANITY_DATASET');
 
 export const projectId = assertEnvVar(PUBLIC_SANITY_PROJECT_ID, 'PUBLIC_SANITY_PROJECT_ID');
+
+export const apiVersion = PUBLIC_SANITY_API_VERSION || '2024-03-15';
+
+export const studioUrl = PUBLIC_SANITY_STUDIO_URL || 'http://localhost:3333';
